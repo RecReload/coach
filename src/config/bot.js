be6a5eb386e60ca1c5902f7db432130295395baa
XxgamingxXyt12 <158865391+XxgamingxXyt12@users.noreply.github.com>
2026-08-21 11:16:46 +0100

    Change bot activity type from 1 to 0

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
       {
         name: "RecReload", // required by Discord API, not shown in the client
         state: "playing RecReload",     // this is what people actually see
-        type: 1,               // Custom
+        type: 0,               // Custom
       },
     ],
   },
